6b24930a29d464fc7890956aa56a2f98554fce1a
ИС-23-19Б <arsenkuat84@example.com>
2024-11-25 10:00:41 +0500

    interface

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<div class="container">
+  <div class="row">
+    <div class="col">
+<div class="mb-3">
+  <label for="name" class="form-label">Название </label>
+  <input type="text" class="form-control" id="name">
+</div>
+</template>
+
+<style scoped>
+
+</style>
